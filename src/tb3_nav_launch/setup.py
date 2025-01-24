13ab8d5572c86ddea3cb5d2ed2f82b4002e5e8f3
Vinh Nguyen <ngtv0404@gmail.com>
2025-01-24 11:41:13 +1100

    add launch files for Turtlebot navigation

---
+++ src/tb3_nav_launch/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+import os
+from glob import glob
+
+package_name = 'tb3_nav_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'map'), glob(os.path.join('map', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='itsmevjnk',
+    maintainer_email='ngtv0404@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
