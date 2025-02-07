dc41404516c096ac8adc54a72b07f3e488b89db1
Vinh Nguyen <ngtv0404@gmail.com>
2025-02-07 14:13:42 +1100

    use kickstart timer, seems to help with running high speed simulations

--- src/benchmark_tools/benchmark_tools/timer.py
+++ src/benchmark_tools/benchmark_tools/timer.py
@@ -6,11 +6,15 @@ class Timer(Node):
         super().__init__('timer')
 
         self.duration = self.declare_parameter('duration', 0.0).get_parameter_value().double_value
-        self.get_logger().info(f'waiting for {self.duration} sec')
+        self.kickstart_timer = self.create_timer(0.1, self.start_timer_cb)
+
+    def start_timer_cb(self):
+        self.get_logger().info(f'waiting for {self.duration} sec - starting at {self.get_clock().now().nanoseconds}')
         self.create_timer(self.duration, self.timer_cb)
+        self.kickstart_timer.cancel()
 
     def timer_cb(self):
-        self.get_logger().info(f'time reached, exiting')
+        self.get_logger().info(f'time reached (at {self.get_clock().now().nanoseconds}), exiting')
         raise SystemExit
 
 def main():
